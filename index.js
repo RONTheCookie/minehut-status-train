8f1f01d0e152db5e1dd325a82e00af9e4f86f07c
ronthecookie <ronthecookie0101@gmail.com>
2018-01-06 09:38:02 +0200

    inital source upload

---
+++ index.js
@@ -0,0 +1,43 @@
+/* Made by RONTheCookie - ronthecookie.me */
+const express = require("express");
+const app = express();
+const pongInterval = 300000; // ms
+let status;
+const port = 80;
+const ping = require('minecraft-ping');
+app.get("/", async (req, res) => {
+    if (status) {
+        res.sendFile(__dirname+"/pages/yes.html");
+    } else {
+        res.sendFile(__dirname+"/pages/no.html");
+    }
+});
+check();
+async function check(){
+    let p = await pong();
+    status = p.playersOnline > 1;
+}
+setInterval(check, pongInterval);
+app.listen(port, ()=>{
+    console.log("Listening on port "+port);
+});
+/**
+ * @typedef {Object} PongResponse
+ * @property {Number} pingVersion the ping version
+ * @property {Number} protocolVersion
+ * @property {String} gameVersion
+ * @property {String} motd
+ * @property {Number} playersOnline
+ * i dont care about maxplayers 
+ */
+/**
+ * @returns {Promise<PongResponse>} the response
+ */
+function pong() {
+    return new Promise((resolve, reject) => {
+        ping.ping_fe01fa({host: "minehut.com", port:25565}, (err, response) => {
+            if (err) reject(err);
+            resolve(response);
+        });
+    });
+}
